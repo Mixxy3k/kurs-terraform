9007d702fc7fbef2537b95e6ecd2c1eda94c7860
Artur Szandrocha <artur.szandrocha@infopower.pl>
2024-11-26 14:42:57 +0100

    Refactor Terraform configuration to check for existing Docker network and create it if absent

--- main.tf
+++ main.tf
@@ -1,19 +1,21 @@
 terraform {
-  # Definicja wymaganego dostawcy (provider) Docker!
   required_providers {
     docker = {
-      source  = "kreuzwerker/docker" # Źródło dostawcy Terraform dla Dockera
-      version = "~> 2.25"            # Wersja dostawcy Docker
+      source  = "kreuzwerker/docker"
+      version = "~> 2.25"
     }
   }
 }
+
 # Ustawienie dostawcy Docker
 provider "docker" {}
 
+# Pobranie informacji o istniejącej sieci
 data "docker_network" "existing_network" {
   name = "app_network"
 }
 
+# Utworzenie sieci dla aplikacji jeśli nie istnieje
 resource "docker_network" "app_network" {
   count = length(data.docker_network.existing_network.id) > 0 ? 0 : 1
   name  = "app_network"
@@ -21,66 +23,68 @@ resource "docker_network" "app_network" {
 
 # Wolumin dla MongoDB
 resource "docker_volume" "mongo_data" {
-    name = "mongo_data"
+  name = "mongo_data"
 }
 
 # Pobranie obrazu z DockerHub
 resource "docker_image" "mongo" {
-    name = "mongo:5.0"
+  name = "mongo:5.0"
 }
 
-# Tworzenie kontenera dla MongoDB 
 # Tworzenie kontenera dla MongoDB
 resource "docker_container" "mongo" {
-    name  = "mongo"
-    image = docker_image.mongo.name
+  name  = "mongo"
+  image = docker_image.mongo.name
 
-    ports {
-        internal = 27017 # port w kontenerze
-        external = 27017 # port w hoście 
-    }
-    networks_advanced {
-        name = docker_network.app_network.name
-    }
-    mounts {
-        target = "/data/db"
-        source = docker_volume.mongo_data.name
-        type   = "volume"
-    }
+  ports {
+    internal = 27017
+    external = 27017
+  }
 
-    healthcheck {
-        test     = ["CMD", "mongo", "--eval", "db.runCommand({ ping: 1 })"]
-        interval = "10s"
-        retries  = 3
-    }
+  networks_advanced {
+    name = length(docker_network.app_network) > 0 ? docker_network.app_network[0].name : data.docker_network.existing_network.name
+  }
+
+  mounts {
+    target = "/data/db"
+    source = docker_volume.mongo_data.name
+    type   = "volume"
+  }
+
+  healthcheck {
+    test     = ["CMD", "mongo", "--eval", "db.runCommand({ ping: 1 })"]
+    interval = "10s"
+    retries  = 3
+  }
 }
 
 # Tworzenie obrazu Dockera dla Nodejs
 resource "docker_image" "app_image" {
-    name = "node-mongo-app"
-    build {
-        context = "${path.module}/."
-        dockerfile = "Dockerfile"
-    }
+  name = "node-mongo-app"
+  build {
+    context    = "${path.module}/."
+    dockerfile = "Dockerfile"
+  }
 }
-# Tworzenie kontenera dla aplikacji Nodejs
+
 # Tworzenie kontenera dla aplikacji Nodejs
 resource "docker_container" "app" {
-    name   = "app" # Nazwa kontenera
-    image  = docker_image.app_image.name # Wykorzystanie obrazu lokalnego
-    ports {
-        internal = 3000 # port w kontenerze
-        external = 3000 # port w hoście 
-    }
-    networks_advanced {
-        name = docker_network.app_network.name
-    }
-    env = [
-        "MONGO_HOST=mongo",
-        "MONGO_PORT=27017"
-    ]
-    # Kontener aplikacji nie uruchomi się przed Mongo
-    depends_on = [docker_container.mongo] 
+  name   = "app"
+  image  = docker_image.app_image.name
+
+  ports {
+    internal = 3000
+    external = 3000
+  }
+
+  networks_advanced {
+    name = length(docker_network.app_network) > 0 ? docker_network.app_network[0].name : data.docker_network.existing_network.name
+  }
+
+  env = [
+    "MONGO_HOST=mongo",
+    "MONGO_PORT=27017"
+  ]
+
+  depends_on = [docker_container.mongo]
 }
-
-
